c26d638f025a7f37c86dbf2667d8c6f9469d2baf
Vitalii Zakharchuk <vit.zkh@gmail.com>
2023-08-09 14:44:43 +0300

    Port

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as express from 'express'
 import { join } from 'path'
+import * as process from "process";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { cors: false })
 	app.enableCors({ credentials: true, origin: true })
 	app.use('/uploads', express.static(join(__dirname, '..', '/uploads')))
 
-	await app.listen(4000)
+	await app.listen(process.env.PORT, '0.0.0.0')
 }
 
 bootstrap()
